0217cccce8050cbb7b1da0b8d7ec431a86b91f8d
Mauricio Morgado <mauriciomorgado@gmail.com>
2018-06-18 19:48:39 -0500

    asdasd

--- src/test/java/mx/unam/dgpe/sample/controller/TestCalculadora.java
+++ src/test/java/mx/unam/dgpe/sample/controller/TestCalculadora.java
@@ -9,45 +9,45 @@ import static mx.unam.dgpe.sample.controller.RestUtil.*;
 
 public class TestCalculadora extends AbstractVerticle {
     private static final Logger logger = Logger.getLogger(TestCalculadora.class);
-    
+
     //@Test
     //public void ok() throws Exception {
 //        String result = sendGet("https://www.binance.com/api/v3/ticker/price?symbol=BTCUSDT");
   //      logger.info(result);
     //}
 
-    
-    @Test
+
+   // @Test
     public void fact() throws Exception {
 	/*
-    	if(false){  
+    	if(false){
       String result = sendGet("http://127.0.0.1:8080/api/factorial?b=5");
         logger.info(result);
         assertTrue("de ",result.contains("3"));
-        
+
          result = sendGet("http://127.0.0.1:8080/api/factorial");
         logger.info(result);
         assertTrue("de ",result.contains("-1"));
-        
+
          result = sendGet("http://127.0.0.1:8080/api/factorial?b=5a");
         logger.info(result);
         assertTrue("de ",result.contains("-1"));
-        
+
          result = sendGet("http://127.0.0.1:8080/api/factorial?b=0");
         logger.info(result);
         assertTrue("de ",result.contains("1"));
-        
-        
+
+
          result = sendGet("http://127.0.0.1:8080/api/factorial?b=1");
         logger.info(result);
         assertTrue("de ",result.contains("1"));
-        
-        
+
+
         result = sendGet("http://127.0.0.1:8080/api/factorial?b=10");
         logger.info(result);
         assertTrue("de ",result.contains("7"));
-        
-        
+
+
         result = sendGet("http://127.0.0.1:8080/api/factorial?b=158");
         logger.info(result);
         assertTrue("de ",result.contains("281"));
@@ -57,8 +57,8 @@ public class TestCalculadora extends AbstractVerticle {
 
    /* @Test
     public void suma() throws Exception {
-	
-    	if(true){  
+
+    	if(true){
       String result = sendGet("http://127.0.0.1:8081/api/suma?a=1&b=2");
         logger.info(result);
         assertTrue("con bvg",result.contains("3"));
